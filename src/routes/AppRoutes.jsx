163bc9748e4c404a2a10ec8924ce0921dcb03e50
r_sunrise <rozasun61@gmail.com>
2025-08-28 18:54:40 +0700

    config Route and config Type Alias

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "@/Page/Home";
+import About from "@/Page/AboutMe";
+import Skills from "@/Page/Skills";
+import Projects from "@/Page/Projects";
+import Contact from "@/Page/Contact";
+import Education from "@/Page/Education";
+import Experience from "@/Page/Experience";
+import Testimonials from "@/Page/Testimonials";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/education" element={<Education />} />
+      <Route path="/experience" element={<Experience />} />
+      <Route path="/testimonials" element={<Testimonials />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
